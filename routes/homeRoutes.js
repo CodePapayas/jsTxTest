b8f48475983e44735b6ddca6bb136f04b1b22f8e
zacholson <codepapayas@outlook.com>
2023-07-19 04:14:58 -0700

    Create index.html as home page; remove redundent / unsued files

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,7 +3,15 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+router.get('/clientForm.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../clientForm.html'));
+});
+
+router.get('/form.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '../form.html'));
 });
 
 module.exports = router;
